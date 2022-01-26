f42572968fd07aae4e1c9a01e855c986eaedb207
Magdalena Sojka <sojka.magdalena.90@gmail.com>
2022-01-26 19:33:23 +0100

    Add randomNumber

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
-var computerMove, playerMove;
-computerMove = 'kamień';
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-playerMove = 'papier';
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else if (randomNumber == '2') {
+  computerMove = 'nożyce';
+} else if (randomNumber == '3') {
+  computerMove = 'papier';
+}
+printMessage('Mój ruch: ' + computerMove);
+computerMove = 'nieznany ruch';
